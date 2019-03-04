df7e8c9e275b6324164bb123c0773cf0b235f025
sakydogalo <sakydogalo@gmail.com>
2019-03-05 00:32:46 +0100

    PASO 1

---
+++ Makefile
@@ -0,0 +1,2 @@
+ejecutable: main.c  
+	gcc -Wall -o ejecutable main.c cola.c pila.c listas.c -I cola.h pila.h listas.h
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "cola.h"
+#include "pila.h"
+#include "listas.h"
+
+int main(int argc, char** argv) {
+
+
+	//fin del programa
+	exit(0);
+
+}
