1270dbcca08ea75247182e4685befc4dc89187e6
sakydogalo <sakydogalo@gmail.com>
2019-03-11 16:07:21 +0100

    Desarrollo del menu

--- Makefile
+++ Makefile
@@ -1,14 +1,21 @@
+INCLUDES =-I TAD\
+
+LIBINCLUDES =-L TAD\
+
+INC = $(INCLUDES) $(LIBINCLUDES)
+
 ejecutable: main.o cola.o pila.o listas.o
-	gcc -Wall -o ejecutable main.o cola.o pila.o listas.o 
+		gcc -Wall -o ejecutable main.o cola.o pila.o listas.o 
 
 cola.o: TAD\cola.c TAD\cola.h
-		gcc -c TAD\cola.c -c
+		gcc -c TAD\cola.c
 
 pila.o: TAD\pila.c TAD\pila.h
-		gcc -c TAD\pila.c -c
+
+		gcc -c TAD\pila.c
 
 listas.o: TAD\listas.c TAD\listas.h
-		gcc -c TAD\listas.c -c
+		gcc -c TAD\listas.c
 		
 clean:
 	rm *.o
--- main.c
+++ main.c
@@ -6,7 +6,39 @@
 #include "TAD\listas.h"
 
 int main(int argc, char** argv) {
-
+	//variables
+	char opcion;
+	//menu
+    do {
+        printf("\n--------------------\n");
+        printf("\na) Anotarse a la cola");
+        printf("\nb) Atender cliente");
+        printf("\nc) Cobrar pluses");
+        printf("\nd) Obtener estadisticas");
+        printf("\ne) Salir");
+        printf("\n--------------------\n");
+        printf("\nOpcion: ");
+        scanf(" %c", &opcion);
+        switch (opcion) {
+            case 'a':
+				printf("Anotando en la cola\n");
+                break;
+            case 'b':
+				printf("Atendendiendo cliente\n");
+				break;
+            case 'c':
+				printf("Cobrando pluses\n");
+                break;            
+            case 'd':
+				printf("Obteniendo estadisticas\n");
+                break;
+            case 'e':
+                printf("Saliendo del programa\n");
+                break;
+            default: printf("Opcion incorrecta\n");
+        }
+    } while (opcion != 'e');
+    return (EXIT_SUCCESS);	
 
 	//fin del programa
 	exit(0);
